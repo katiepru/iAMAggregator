dea93cbdac7d62e766fbb580d5654a109adf934e
katiepru <katiepru@nbcs.rutgers.edu>
2013-03-31 07:15:52 -0400

    working on reddit stuff

--- helpers.py
+++ helpers.py
@@ -1,9 +1,17 @@
 import pymongo
 import praw
-from config import *
+import json
+#from config import *
 
 reddit = praw.Reddit(user_agent='iAMAggregator scanning for new content.')
 subreddit = reddit.get_subreddit("iama")
+submissions = subreddit.get_top(limit=10)
 
 dbconnection = pymongo.Connection()
 db = dbconnection.iamaggregator
+collect = db.people
+
+sub = submissions
+while sub != None:
+	sub = sub.next
+	print sub.comments[0]
