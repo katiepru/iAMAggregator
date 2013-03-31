6aef1743bf6c3358b1693484810918070535e273
Josh Matthews <josh@jmatthews.us>
2013-03-31 07:16:19 -0400

    Starting to add ama page.

--- application.py
+++ application.py
@@ -6,8 +6,13 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    top_posts = None
+    top_posts = {}
     return render_template('main.html', top_posts=top_posts)
+
+@app.route('/ama/<id>')
+def ama(post_id):
+    responses = get_responses(limit=10)
+    return render_template('ama.html', responses)
 
 if __name__ == '__main__':
 	app.run(debug=True)
