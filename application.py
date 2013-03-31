89a9125567aec7a5e59649bb8a8cf91b238611ef
Josh Matthews <josh@jmatthews.us>
2013-03-31 10:04:34 -0400

    Adding function for getting ama pages.

--- application.py
+++ application.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask import render_template
 from flask import request
 from helpers import *
+import pdb
 
 app = Flask(__name__)
 
@@ -11,13 +12,13 @@ def index():
     top_qas = get_teasers()
     print(dir(top_qas))
     print(dir(top_posts))
+    pdb.set_trace()
     return render_template('main.html', top_posts=top_posts, top_qas=top_qas)
 
-@app.route('/ama/<id>')
+@app.route('/ama/<post_id>')
 def ama(post_id):
-    responses = get_responses(limit=10)
-    print(dir(responses))
-    return render_template('ama.html', responses)
+    responses = get_responses(post_id, limit=20)
+    return render_template('ama.html', responses=responses)
 
 if __name__ == '__main__':
 	app.run(debug=True)
