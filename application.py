338ef7843dd439c011a8e365f35deade1ddb0174
katiepru <katiepru@nbcs.rutgers.edu>
2013-03-31 10:13:39 -0400

    mostly working woo

--- application.py
+++ application.py
@@ -12,13 +12,16 @@ def index():
     top_qas = get_teasers()
     print(dir(top_qas))
     print(dir(top_posts))
-    pdb.set_trace()
     return render_template('main.html', top_posts=top_posts, top_qas=top_qas)
 
 @app.route('/ama/<post_id>')
 def ama(post_id):
     responses = get_responses(post_id, limit=20)
-    return render_template('ama.html', responses=responses)
+    top_posts = get_top_posts()
+    for i in responses:
+		print i
+		print "------------------------"
+    return render_template('ama.html', responses=responses, top_posts=top_posts)
 
 if __name__ == '__main__':
 	app.run(debug=True)
